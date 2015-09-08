068c9c2a9a9ff06b0e7e87e229832929c7fcd5dd
Yuval Wyborski <yvw.bor@gmail.com>
2015-09-08 09:13:19 +0300

    Add project files.

---
+++ board.js
@@ -0,0 +1,5 @@
+/****************
+ * Will be used to hold a board prototype.
+ * Which remembers the position, and checks for legal moves
+ *
+ ****************
---
+++ pieces.js
@@ -0,0 +1,9 @@
+/****************
+ * Will be used to hold the pieces prototype.
+ * pP - pawn
+ * bB - bishop
+ * rR - rook
+ * nN - knight
+ * qQ - queen
+ * kK - king
+ ****************
